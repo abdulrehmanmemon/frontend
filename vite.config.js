50f4f2234d746eea5295ad5554161931ec94ffda
Abdul Rehman <110313396+abdulrehmanmemon@users.noreply.github.com>
2025-04-18 15:39:23 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', 
-    port: 5173,  
-    watch: {
-      usePolling: true,
-    },    
-  },
   resolve: {
     alias: {
-      "@": path.resolve(path.resolve(), "src"), 
+      "@": path.resolve(__dirname, "src"), 
+    },
   },
-}});
+  base: process.env.VITE_BASE_PATH || "/"
+});
